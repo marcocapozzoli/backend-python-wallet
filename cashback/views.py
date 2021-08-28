b7720b7b3e90de49c3adf027ca367841f522c403
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-08-28 14:43:43 -0300

    created the viewsets

--- cashback/views.py
+++ cashback/views.py
@@ -1,4 +1,27 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
+
+from cashback.models import Customer, Product, Buy
+from cashback.serializers import BuySerializer, ProductSerializer, CustomerSerializer
 
 
+class BuyViewSet(viewsets.ModelViewSet):
+    serializer_class = BuySerializer
+    
+    def get_queryset(self):
+        buy = Buy.objects.all()
+        return buy
+   
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    
+    def get_queryset(self):
+        product = Product.objects.all()
+        return product
+    
+class CustomerViewSet(viewsets.ModelViewSet):
+    serializer_class = CustomerSerializer
+    
+    def get_queryset(self):
+        customer = Customer.objects.all()
+        return customer
+    
--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cashback.urls')),
+    path('api/', include('cashback.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
