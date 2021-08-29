36991c85db5d0385d949fa4597193db8d41ee133
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-08-28 21:21:12 -0300

    update cashback.urls

--- cashback/urls.py
+++ cashback/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path,include
+
+from rest_framework.routers import DefaultRouter
 
 from cashback import views
 
+
+router = DefaultRouter()
+router.register('buy', views.BuyViewSet, basename='buy')
+router.register('product', views.ProductViewSet, basename='product')
+router.register('customer', views.CustomerViewSet, basename='customer')
+
 urlpatterns = [
-    path('', views.api_root, name='api-root'),
+    path('', include(router.urls)),
 ]
