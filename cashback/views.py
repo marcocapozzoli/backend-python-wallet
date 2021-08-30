5b20ccefc034c40a59c5630ede6f42e6febe99ff
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-08-30 00:05:19 -0300

    override create method in BuyViewSet

--- cashback/views.py
+++ cashback/views.py
@@ -1,15 +1,29 @@
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from cashback.api import call_api_maisTodos
 
 from cashback.models import Customer, Product, Buy
 from cashback.serializers import BuySerializer, ProductSerializer, CustomerSerializer
 
 
 class BuyViewSet(viewsets.ModelViewSet):
-    serializer_class = BuySerializer
+    queryset = Buy.objects.all()
+    serializer_class = BuySerializer    
     
-    def get_queryset(self):
-        buy = Buy.objects.all()
-        return buy
+    def create(self, request, *args, **kwargs):
+        serializer = BuySerializer(data=request.data)
+        serializer.is_valid(self)
+        serializer.save()
+        
+        buys = self.queryset.order_by('-id')
+        last_buy = buys[0]
+        document = last_buy.customer.cpf
+        cashback = last_buy.cashback
+        
+        response_api_maisTodos = call_api_maisTodos(document,cashback)
+        
+        return Response(response_api_maisTodos, status=status.HTTP_201_CREATED)
    
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
