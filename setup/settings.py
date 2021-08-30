c43f49b7c720a074580aa51a7c6b75a90b1b7438
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-08-30 16:34:53 -0300

    Add configuration for token authentication

--- setup/settings.py
+++ setup/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     
     # 3rd apps
     'rest_framework',
+    'rest_framework.authtoken',
+    'drf_yasg',
     
     # Local apps
     'cashback',
@@ -118,3 +120,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
