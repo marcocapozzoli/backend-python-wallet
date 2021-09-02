18938d4e3c574aaa8c88b716a3a030eb0e919ac5
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-09-02 09:44:17 -0300

    add CustomLoginView

--- users/views.py
+++ users/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from .forms import SignUpForm
 
 
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
 class SignUpView(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy('signup')
-    template_name = 'signup.html'
+    template_name = 'users/signup.html'
