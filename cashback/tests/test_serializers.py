f59d792235cec608b6bc89bde800fa0d00d6e47e
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-09-02 22:27:41 -0300

    tests fields BuySerializer

--- cashback/tests/test_serializers.py
+++ cashback/tests/test_serializers.py
@@ -49,3 +49,46 @@ class ProductSerializerTestCase(APITestCase):
         self.assertEqual(data['type'], self.product.type)
         self.assertEqual(data['price'], self.product.price)
         self.assertEqual(data['quantity'], self.product.quantity)
+
+class BuySerializerTestCase(APITestCase):
+    
+    def setUp(self):
+        self.customer_attributes = {
+            "name":"maistodos",
+            "cpf":"30314608044"
+        }
+        self.product_attributes = {
+            "description": "product1",
+            "type": "A",
+            "price": 100.55,
+            "quantity": 1
+        }       
+        self.buy_attributes = {
+            "date": "2021-10-29 08:03:10",
+            "amount": 100.55
+        }
+        self.customer = Customer.objects.create(**self.customer_attributes)
+        self.product = Product.objects.create(**self.product_attributes)
+        self.buy = Buy.objects.create(**self.buy_attributes)
+        self.buy.customer = self.customer
+        self.buy.products.add(self.product)
+        self.serializer = BuySerializer(instance=self.buy)
+    
+    def test_buy_contains_expected_fields(self):
+        """test that verifies that fields are being serialized correctly"""
+        data = self.serializer.data
+        
+        self.assertEqual(set(data.keys()), set(['id','date','customer','products','amount','cashback']))
+
+    def test_buy_fields_content(self):
+        """test that verifies that the contents of serialized fields are correct"""
+        data = self.serializer.data
+        
+        for product in data['products']:
+            data_product = product
+        
+        self.assertEqual(data['date'], self.buy.date)
+        self.assertEqual(data['customer'], self.customer_attributes)
+        self.assertEqual(data_product, self.product_attributes)
+        self.assertEqual(data['amount'], self.buy.amount)
+        self.assertEqual(data['cashback'], self.buy.cashback)
