7ea6f2e95ff6379c467e68aadf0d6835e148c0ec
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-09-02 22:26:50 -0300

    tests ProductSerializer

--- cashback/tests/test_serializers.py
+++ cashback/tests/test_serializers.py
@@ -24,3 +24,28 @@ class CustomerSerializerTestCase(APITestCase):
         data = self.serializer.data
         self.assertEqual(data['name'], self.customer.name)
         self.assertEqual(data['cpf'], self.customer.cpf)
+
+class ProductSerializerTestCase(APITestCase):
+    
+    def setUp(self):
+        self.product_attributes = {
+            "description": "product1",
+            "type": "A",
+            "price": 100.55,
+            "quantity": 1
+        }
+        self.product = Product.objects.create(**self.product_attributes)
+        self.serializer = ProductSerializer(instance=self.product)
+            
+    def test_product_contains_expected_fields(self):
+        """test that verifies that fields are being serialized correctly"""
+        data = self.serializer.data
+        self.assertEqual(set(data.keys()), set(['description', 'type', 'price', 'quantity']))
+
+    def test_product_fields_content(self):
+        """test that verifies that the contents of serialized fields are correct"""
+        data = self.serializer.data
+        self.assertEqual(data['description'], self.product.description)
+        self.assertEqual(data['type'], self.product.type)
+        self.assertEqual(data['price'], self.product.price)
+        self.assertEqual(data['quantity'], self.product.quantity)
